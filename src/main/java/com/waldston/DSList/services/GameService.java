c790c872fe28d0f7ab3ed44003d7a154da36bab3
Waldston Santana <waldston.ws@gmail.com>
2023-11-16 19:47:16 -0300

    GET game by id

--- src/main/java/com/waldston/DSList/services/GameService.java
+++ src/main/java/com/waldston/DSList/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.waldston.dslist.dto.GameDTO;
 import com.waldston.dslist.dto.GameMinDTO;
 import com.waldston.dslist.entities.Game;
 import com.waldston.dslist.repositories.GameRepository;
@@ -15,12 +17,23 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		
+		return dto;
+		
+	}
+	
 	public List<GameMinDTO> findAll() {
 		var result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 		
 		
 	}
+	
+
 	
 	
 	
